89eee6d93e9bcc737c6646e9c5839944eba376e3
Andrew Sarver <22250203+heysarver@users.noreply.github.com>
2023-06-28 21:06:29 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,7 +2,7 @@ $.getJSON('https://api.github.com/repos/heysarver/winwalls/contents/wallpapers',
   $.each(data, function(index, item) {
     if(item.type === 'file') {
       var imageUrl = item.download_url;
-      var imageElement = $('<a href="' + imageUrl + '" data-lightbox="gallery"><img src="' + imageUrl + '" width="200"></a>');
+      var imageElement = $('<a href="' + imageUrl + '" data-lightbox="gallery"><img src="' + imageUrl + '" width="25%"></a>');
       $('#gallery').append(imageElement);
     }
   });
